3d3dc3676dad1ab6f657bd139d3f47de4c822c12
Marc Diechler <marc.diechler@student.nhh.no>
2025-09-24 09:32:16 +0200

    implement argmax function

--- lectures/lecture1/template/lecture1-MD.py
+++ lectures/lecture1/template/lecture1-MD.py
@@ -18,3 +18,19 @@ Tasks
 4.  Use the notebook lecture1-benchmark.ipynb to benchmark your implementation 
     against NumPy's argmax().
 """
+
+import numpy as np
+def argmax(lst):
+    N = len(lst)
+    imax = 0
+    value_max = - np.inf
+    for i in range(N):
+        value = lst[i]
+        if value > value_max:
+            imax = 1 
+            value_max = value
+    return imax 
+
+values = [2, 3, -1, 7, 4]
+i = argmax(values)
+print(f"Index of maximum value: {i}")
